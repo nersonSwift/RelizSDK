6881cf73e9866fd6fd66f17837e9f9bea842b932
nersonSwift <42254802+nersonSwift@users.noreply.github.com>
2020-10-14 15:35:14 +0300

    Initial Commit

---
+++ RelizKit/RelizKit.h
@@ -0,0 +1,18 @@
+//
+//  RelizKit.h
+//  RelizKit
+//
+//  Created by Александр Сенин on 14.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RelizKit.
+FOUNDATION_EXPORT double RelizKitVersionNumber;
+
+//! Project version string for RelizKit.
+FOUNDATION_EXPORT const unsigned char RelizKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RelizKit/PublicHeader.h>
+
+
