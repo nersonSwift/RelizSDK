d013b9d24cc64f9470b405e5212d1d3f5ba4e5d9
nersonSwift <42254802+nersonSwift@users.noreply.github.com>
2020-12-10 20:41:28 +0300

    UIKit фикс хереда

--- RZUIKit/RZUIKit/RZUIKit/RZUIKit.h
+++ RZUIKit/RZUIKit/RZUIKit/RZUIKit.h
@@ -7,6 +7,16 @@
 
 #import <Foundation/Foundation.h>
 
-#import <RZDarkModeKit/RZDarkModeKit.h>
-#import <RZScreensKit/RZScreensKit.h>
-#import <RZViewBuilder/RZViewBuilder.h>
+#if !defined(__has_include)
+    #error "Firebase.h won't import anything if your compiler doesn't support __has_include. Please \ import the headers individually."
+#else
+    #if __has_include(<RZDarkModeKit/RZDarkModeKit.h>)
+      #import <RZDarkModeKit/RZDarkModeKit.h>
+    #endif
+    #if __has_include(<RZScreensKit/RZScreensKit.h>)
+      #import <RZScreensKit/RZScreensKit.h>
+    #endif
+    #if __has_include(<RZViewBuilder/RZViewBuilder.h>)
+      #import <RZViewBuilder/RZViewBuilder.h>
+    #endif
+#endif
