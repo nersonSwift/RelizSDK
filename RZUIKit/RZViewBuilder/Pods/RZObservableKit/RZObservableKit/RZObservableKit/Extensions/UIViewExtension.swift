db320e120ad29e99efd8b342aac5fa87b715172a
Александр Сенин <>
2021-03-24 13:43:50 +0300

    no message

--- RZUIKit/RZViewBuilder/Pods/RZObservableKit/RZObservableKit/RZObservableKit/Extensions/UIViewExtension.swift
+++ RZUIKit/RZViewBuilder/Pods/RZObservableKit/RZObservableKit/RZObservableKit/Extensions/UIViewExtension.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 extension UIView{
+    private var rzFrameKey: String {"frame"}
+    public var rzFrame: RZObservable<CGRect>{
+        if let rzFrameO = Associated(self).get(.hashable(rzFrameKey)) as? RZUIViewFrameObserve{
+            return rzFrameO.$rzFrame
+        }else{
+            let rzFrameO = RZUIViewFrameObserve(self)
+            Associated(self).set(rzFrameO, .hashable(rzFrameKey), .OBJC_ASSOCIATION_RETAIN)
+            return rzFrameO.$rzFrame
+        }
+    }
+    
     public static var isAnimation: Bool {
         let view = UIView()
         view.alpha = 0
@@ -15,3 +26,29 @@ extension UIView{
     }
 }
 
+class RZUIViewFrameObserve{
+    private var keys: [NSKeyValueObservation?] = []
+    @RZObservable var rzFrame: CGRect = .zero
+    private weak var view: UIView?
+    
+    init(_ view: UIView) {
+        self.view = view
+        rzFrame = view.frame
+        setObserve(view)
+    }
+    
+    private func setObserve(_ view: UIView){
+        keys.append(view.observe(\.frame, options: [.old, .new]) {[weak self] (view, value) in
+            if value.oldValue == value.newValue {return}
+            self?.rzFrame = view.frame
+        })
+        keys.append(view.observe(\.center, options: [.old, .new]) {[weak self] (view, value) in
+            if value.oldValue == value.newValue {return}
+            self?.rzFrame = view.frame
+        })
+        keys.append(view.observe(\.bounds, options: [.old, .new]) {[weak self] (view, value) in
+            if value.oldValue == value.newValue {return}
+            self?.rzFrame = view.frame
+        })
+    }
+}
