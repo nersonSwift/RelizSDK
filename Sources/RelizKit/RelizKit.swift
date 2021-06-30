d6a9a24ea52ca935cbde872f378634ce677cf650
Александр Сенин <>
2021-06-30 22:57:32 +0300

    2.0.19

--- Sources/RelizKit/RelizKit.swift
+++ Sources/RelizKit/RelizKit.swift
@@ -3,10 +3,26 @@
 @_exported import UIKit
 @_exported import SwiftUI
 
+#if canImport(RZEvent)
 @_exported import RZEvent
-@_exported import RZViewBuilder
-@_exported import RZStoreKit
+#endif
 
-@_exported import RZUIPacKit
+#if canImport(RZEvent)
 @_exported import RZViewBuilder
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZStoreKit
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZUIPacKit
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZViewBuilder
+#endif
+
+#if canImport(RZEvent)
 @_exported import RZDarkModeKit
+#endif
