f96f24dac3e96b7644c211b7099dd8a180520b95
Александр Сенин <>
2021-06-30 22:46:40 +0300

    2.0.17

--- Package.swift
+++ Package.swift
@@ -3,6 +3,95 @@
 
 import PackageDescription
 
+//let package = Package.init(
+//    name: "RelizSDK",
+//    platforms: [.iOS(.v13)],
+//    products: [
+//        .library(
+//            name: "RZEvent",
+//            targets: ["RZEvent"]
+//        ),
+//        .library(
+//            name: "RZObservableKit",
+//            targets: ["RZObservableKit"]
+//        ),
+//        .library(
+//            name: "RZStoreKit",
+//            targets: ["RZStoreKit"]
+//        ),
+//        .library(
+//            name: "RZDarkModeKit",
+//            targets: ["RZDarkModeKit"]
+//        ),
+//        .library(
+//            name: "RZUIPacKit",
+//            targets: ["RZUIPacKit"]
+//        ),
+//        .library(
+//            name: "RZViewBuilder",
+//            targets: ["RZViewBuilder"]
+//        )
+//    ],
+//    dependencies: [
+//        .package(
+//            name: "SwiftyStoreKit",
+//            url: "https://github.com/bizz84/SwiftyStoreKit.git",
+//            from: "0.16.3"
+//        )
+//    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "RelizKit",
+//            dependencies: [],
+//            path: "Sources/RelizKit",
+//            exclude: ["Info.plist"],
+//            cSettings: [
+//                .headerSearchPath("Sources/RelizKit")
+//            ]
+//        ),
+//        .target(
+//            name: "RZEvent",
+//            dependencies: [],
+//            exclude: ["Info.plist"]
+//        ),
+//        .target(
+//            name: "RZObservableKit",
+//            dependencies: [],
+//            exclude: ["Info.plist"]
+//        ),
+//        .target(
+//            name: "RZStoreKit",
+//            dependencies: ["SwiftyStoreKit"],
+//            exclude: ["Info.plist"]
+//        ),
+//        .target(
+//            name: "RZDarkModeKit",
+//            dependencies: [],
+//            path: "Sources/RZUIKit/RZDarkModeKit",
+//            exclude: ["Info.plist"]
+//        ),
+//        .target(
+//            name: "RZUIPacKit",
+//            dependencies: ["RZObservableKit"],
+//            path: "Sources/RZUIKit/RZUIPacKit",
+//            exclude: ["Info.plist"]
+//        ),
+//        .target(
+//            name: "RZViewBuilder",
+//            dependencies: ["RZObservableKit"],
+//            path: "Sources/RZUIKit/RZViewBuilder",
+//            exclude: ["Info.plist"]
+//        )
+//    ]
+//)
+
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
 let package = Package.init(
     name: "RelizSDK",
     platforms: [.iOS(.v13)],
@@ -48,7 +137,7 @@ let package = Package.init(
             path: "Sources/RelizKit",
             exclude: ["Info.plist"],
             cSettings: [
-                .headerSearchPath("Sources/RelizKit")
+                .headerSearchPath("Sources/RelizKi")
             ]
         ),
         .target(
@@ -86,4 +175,3 @@ let package = Package.init(
         )
     ]
 )
-
--- Sources/RelizKit/RelizKit.swift
+++ Sources/RelizKit/RelizKit.swift
@@ -3,26 +3,10 @@
 @_exported import UIKit
 @_exported import SwiftUI
 
-#if canImport(RZEvent)
-    @_exported import RZEvent
-#endif
+@_exported import RZEvent
+@_exported import RZViewBuilder
+@_exported import RZStoreKit
 
-#if canImport(RZViewBuilder)
-    @_exported import RZViewBuilder
-#endif
-
-#if canImport(RZStoreKit)
-    @_exported import RZStoreKit
-#endif
-
-#if canImport(RZUIPacKit)
-    @_exported import RZUIPacKit
-#endif
-
-#if canImport(RZViewBuilder)
-    @_exported import RZViewBuilder
-#endif
-
-#if canImport(RZDarkModeKit)
-    @_exported import RZDarkModeKit
-#endif
+@_exported import RZUIPacKit
+@_exported import RZViewBuilder
+@_exported import RZDarkModeKit
