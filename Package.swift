473531f68bfd0d952cffcddee2dbba85d9f9699d
Александр Сенин <>
2021-07-01 17:52:22 +0300

    2.0.25

--- Package.swift
+++ Package.swift
@@ -3,6 +3,90 @@
 
 import PackageDescription
 
+
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+let package = Package.init(
+  name: "RelizSDK",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(
+      name: "RZEvent",
+      targets: ["RZEvent"]
+    ),
+    .library(
+      name: "RZObservableKit",
+      targets: ["RZObservableKit"]
+    ),
+    .library(
+      name: "RZStoreKit",
+      targets: ["RZStoreKit"]
+    ),
+    .library(
+      name: "RZDarkModeKit",
+      targets: ["RZDarkModeKit"]
+    ),
+    .library(
+      name: "RZUIPacKit",
+      targets: ["RZUIPacKit"]
+    ),
+    .library(
+      name: "RZViewBuilder",
+      targets: ["RZViewBuilder"]
+    )
+  ],
+  dependencies: [
+    .package(
+      name: "SwiftyStoreKit",
+      url: "https://github.com/bizz84/SwiftyStoreKit.git",
+      from: "0.16.3"
+    )
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "RelizKit",
+      dependencies: [],
+      exclude: ["Info.plist"]
+    ),
+    .target(
+      name: "RZEvent",
+      dependencies: [.target(name: "RelizKit")],
+      exclude: ["Info.plist"]
+    ),
+    .target(
+      name: "RZObservableKit",
+      dependencies: [.target(name: "RelizKit")],
+      exclude: ["Info.plist"]
+    ),
+    .target(
+      name: "RZStoreKit",
+      dependencies: ["SwiftyStoreKit", .target(name: "RelizKit")],
+      exclude: ["Info.plist"]
+    ),
+    .target(
+      name: "RZDarkModeKit",
+      dependencies: [.target(name: "RelizKit")],
+      path: "Sources/RZUIKit/RZDarkModeKit",
+      exclude: ["Info.plist"]
+    ),
+    .target(
+      name: "RZUIPacKit",
+      dependencies: ["RZObservableKit", .target(name: "RelizKit")],
+      path: "Sources/RZUIKit/RZUIPacKit",
+      exclude: ["Info.plist"]
+    ),
+    .target(
+      name: "RZViewBuilder",
+      dependencies: ["RZObservableKit", .target(name: "RelizKit")],
+      path: "Sources/RZUIKit/RZViewBuilder",
+      exclude: ["Info.plist"]
+    )
+  ]
+)
+
 //let package = Package.init(
 //    name: "RelizSDK",
 //    platforms: [.iOS(.v13)],
@@ -90,90 +174,90 @@ import PackageDescription
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
-
-let package = Package.init(
-    name: "RelizSDK",
-    platforms: [.iOS(.v13)],
-    products: [
-        .library(
-            name: "RelizKit",
-            targets: ["RelizKit"]
-        ),
-        .library(
-            name: "RZEvent",
-            targets: ["RZEvent"]
-        ),
-        .library(
-            name: "RZObservableKit",
-            targets: ["RZObservableKit"]
-        ),
-        .library(
-            name: "RZStoreKit",
-            targets: ["RZStoreKit"]
-        ),
-        .library(
-            name: "RZDarkModeKit",
-            targets: ["RZDarkModeKit"]
-        ),
-        .library(
-            name: "RZUIPacKit",
-            targets: ["RZUIPacKit"]
-        ),
-        .library(
-            name: "RZViewBuilder",
-            targets: ["RZViewBuilder"]
-        )
-    ],
-    dependencies: [
-        .package(
-            name: "SwiftyStoreKit",
-            url: "https://github.com/bizz84/SwiftyStoreKit.git",
-            from: "0.16.3"
-        )
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "RelizKit",
-            dependencies: [],
-            path: "Sources/RelizKit",
-            exclude: ["Sources/RelizKit/Info.plist"]
-        ),
-        .target(
-            name: "RZEvent",
-            dependencies: [.target(name: "RelizKit")],
-            exclude: ["Sources/RZEvent/Info.plist"]
-        ),
-        .target(
-            name: "RZObservableKit",
-            dependencies: ["RelizKit"],
-            exclude: ["Sources/RZObservableKit/Info.plist"]
-        ),
-        .target(
-            name: "RZStoreKit",
-            dependencies: ["SwiftyStoreKit", .target(name: "RelizKit")],
-            exclude: ["Sources/RZStoreKit/Info.plist"]
-        ),
-        .target(
-            name: "RZDarkModeKit",
-            dependencies: [.target(name: "RelizKit")],
-            path: "Sources/RZUIKit/RZDarkModeKit",
-            exclude: ["Sources/RZUIKit/RZDarkModeKit/Info.plist"]
-        ),
-        .target(
-            name: "RZUIPacKit",
-            dependencies: ["RZObservableKit", .target(name: "RelizKit")],
-            path: "Sources/RZUIKit/RZUIPacKit",
-            exclude: ["Sources/RZUIKit/RZUIPacKit/Info.plist"]
-        ),
-        .target(
-            name: "RZViewBuilder",
-            dependencies: ["RZObservableKit", .target(name: "RelizKit")],
-            path: "Sources/RZUIKit/RZViewBuilder",
-            exclude: ["Sources/RZUIKit/RZViewBuilder/Info.plist"]
-        )
+//import PackageDescription
+//
+//let package = Package.init(
+//    name: "RelizSDK",
+//    platforms: [.iOS(.v13)],
+//    products: [
+//        .library(
+//            name: "RelizKit",
+//            targets: ["RelizKit"]
+//        ),
+//        .library(
+//            name: "RZEvent",
+//            targets: ["RZEvent"]
+//        ),
+//        .library(
+//            name: "RZObservableKit",
+//            targets: ["RZObservableKit"]
+//        ),
+//        .library(
+//            name: "RZStoreKit",
+//            targets: ["RZStoreKit"]
+//        ),
+//        .library(
+//            name: "RZDarkModeKit",
+//            targets: ["RZDarkModeKit"]
+//        ),
+//        .library(
+//            name: "RZUIPacKit",
+//            targets: ["RZUIPacKit"]
+//        ),
+//        .library(
+//            name: "RZViewBuilder",
+//            targets: ["RZViewBuilder"]
+//        )
+//    ],
+//    dependencies: [
+//        .package(
+//            name: "SwiftyStoreKit",
+//            url: "https://github.com/bizz84/SwiftyStoreKit.git",
+//            from: "0.16.3"
+//        )
+//    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+//        .target(
+//            name: "RelizKit",
+//            dependencies: [],
+//            path: "Sources/RelizKit",
+//            exclude: ["Sources/RelizKit/Info.plist"]
+//        ),
+//        .target(
+//            name: "RZEvent",
+//            dependencies: [.target(name: "RelizKit")],
+//            exclude: ["Sources/RZEvent/Info.plist"]
+//        ),
+//        .target(
+//            name: "RZObservableKit",
+//            dependencies: ["RelizKit"],
+//            exclude: ["Sources/RZObservableKit/Info.plist"]
+//        ),
+//        .target(
+//            name: "RZStoreKit",
+//            dependencies: ["SwiftyStoreKit", .target(name: "RelizKit")],
+//            exclude: ["Sources/RZStoreKit/Info.plist"]
+//        ),
+//        .target(
+//            name: "RZDarkModeKit",
+//            dependencies: [.target(name: "RelizKit")],
+//            path: "Sources/RZUIKit/RZDarkModeKit",
+//            exclude: ["Sources/RZUIKit/RZDarkModeKit/Info.plist"]
+//        ),
+//        .target(
+//            name: "RZUIPacKit",
+//            dependencies: ["RZObservableKit", .target(name: "RelizKit")],
+//            path: "Sources/RZUIKit/RZUIPacKit",
+//            exclude: ["Sources/RZUIKit/RZUIPacKit/Info.plist"]
+//        ),
+//        .target(
+//            name: "RZViewBuilder",
+//            dependencies: ["RZObservableKit", .target(name: "RelizKit")],
+//            path: "Sources/RZUIKit/RZViewBuilder",
+//            exclude: ["Sources/RZUIKit/RZViewBuilder/Info.plist"]
+//        )
 //        .target(
 //            name: "RZEvent",
 //            dependencies: [],
@@ -207,6 +291,6 @@ let package = Package.init(
 //            path: "Sources/RZUIKit/RZViewBuilder",
 //            exclude: ["Info.plist"]
 //        )
-    ]
-)
+//    ]
+//)
 
--- Sources/RelizKit/RelizKit.swift
+++ Sources/RelizKit/RelizKit.swift
@@ -1,29 +1,29 @@
 
-//@_exported import Foundation
-//@_exported import UIKit
-//@_exported import SwiftUI
-//
-//#if canImport(RZEvent)
-//@_exported import RZEvent
-//#endif
-//
-//#if canImport(RZEvent)
-//@_exported import RZViewBuilder
-//#endif
-//
-//#if canImport(RZEvent)
-//@_exported import RZStoreKit
-//#endif
-//
-//#if canImport(RZEvent)
-//@_exported import RZUIPacKit
-//#endif
-//
-//#if canImport(RZEvent)
-//@_exported import RZViewBuilder
-//#endif
-//
-//#if canImport(RZEvent)
-//@_exported import RZDarkModeKit
-//#endif
+@_exported import Foundation
+@_exported import UIKit
+@_exported import SwiftUI
+
+#if canImport(RZEvent)
+@_exported import RZEvent
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZViewBuilder
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZStoreKit
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZUIPacKit
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZViewBuilder
+#endif
+
+#if canImport(RZEvent)
+@_exported import RZDarkModeKit
+#endif
 
