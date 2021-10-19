67070165fa8f0eead56ce82bdd1d006207108e63
Александр Сенин <>
2021-10-19 17:43:00 +0300

    spm test

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "RZEventKit",
+            type: .dynamic,
             targets: ["RZEventKit"]
         ),
 //        .library(
