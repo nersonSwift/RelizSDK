2da85fa5b5cd6fe612c20d6c5449dc3a4e8cc099
Александр Сенин <>
2023-05-11 14:49:01 +0400

    2.5.9

--- Sources/RZUIKit/RZUIEntitiesLib/RZViews/RZUIViewProtocol.swift
+++ Sources/RZUIKit/RZUIEntitiesLib/RZViews/RZUIViewProtocol.swift
@@ -12,19 +12,36 @@ import RZViewBuilderKit
 public protocol RZUIViewProtocol: UIView{
     associatedtype Model: RZUIPacRouter
     var model: Model {get set}
+    
+    func initActions()
     func create()
+}
+
+extension RZUIViewProtocol{
+    public func initActions() {}
 }
 
 extension RZBuilder where V: RZUIViewProtocol{
     @discardableResult
-    public func model(_ closure: (V.Model) -> ()) -> Self{
-        closure(view.model)
+    public func setupModel(_ model: V.Model) -> Self{
+        setupModel{ $0 = model }
+    }
+    
+    @discardableResult
+    public func setupModel(_ closure: (inout V.Model) -> ()) -> Self{
+        closure(&view.model)
+        return initActions()
+    }
+    
+    @discardableResult
+    public func initActions() -> Self{
+        view.initActions()
         return self
     }
     
     @discardableResult
-    public func model(_ model: V.Model) -> Self{
-        view.model = model
+    public func model(_ closure: (V.Model) -> ()) -> Self{
+        closure(view.model)
         return self
     }
     
